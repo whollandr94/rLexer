a7f40e58b3387c9505ff9a6eb167678fd8b43f50
Robert Holland <whollandr94@gmail.com>
2020-09-07 08:39:39 +0100

    Create tags.rb

---
+++ lib/rLexer/tags.rb
@@ -0,0 +1,11 @@
+class Tags
+  IDENTIFIERS = [
+    OPEN_TAG              = '<',
+    CLOSE_TAG             = '>',
+    SELF_CLOSING_TAG      = '/>',
+    CLOSING_TAG           = '</',
+    START_COMMENT         = '<!--',
+    END_COMMENT           = '-->',
+    ATTRIBUTE_ASSIGNMENT  = '='
+  ]
+end
